403b5c09e74c241a4da464027e0919ef0a917dc8
liuao <liuao@xd.com>
2021-07-07 21:33:09 +0800

    2021-07-07

---
+++ git_space/git/src/main/java/git_test.java
@@ -0,0 +1,12 @@
+/**
+ * @author Aol
+ * @create 2021-01-13 18:04
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("我也是醉了");
+        System.out.println("666");
+         System.out.println("6666666");
+    }
+}
